79bc608f32721b601c1646165766a07d245e7390
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-03 09:01:03 +0930

    Make ErrorCode into method (not type)

--- error-code.go
+++ error-code.go
@@ -10,9 +10,12 @@ import "net/http"
 // ErrorCode calls http.Error with the given
 // HTTP status code. It uses http.StatusText
 // for the message.
-type ErrorCode int
+func ErrorCode(code int) http.Handler {
+	return errorCode(code)
+}
 
-// ServeHTTP implements http.Handler.
-func (code ErrorCode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+type errorCode int
+
+func (code errorCode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(int(code)), int(code))
 }
