91c7327ae0ac1eb30624ba2b072880f1a947241a
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-03 09:45:43 +0930

    Add ServeError handler

---
+++ serve-error.go
@@ -0,0 +1,52 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License that can be found in
+// the LICENSE file.
+
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+)
+
+// ServeError returns a http.Handler that serves
+// content with a given HTTP status code.
+//
+// If mimeType is empty, it will be sniffed from
+// content.
+func ServeError(code int, content []byte, mimeType string) http.Handler {
+	if mimeType == "" {
+		mimeType = http.DetectContentType(content)
+	}
+
+	return &serveError{
+		content,
+		strconv.FormatInt(int64(len(content)), 10),
+		mimeType,
+		code,
+	}
+}
+
+type serveError struct {
+	content []byte
+	size    string
+	mime    string
+	code    int
+}
+
+func (s *serveError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h := w.Header()
+	delete(h, "Cache-Control")
+	delete(h, "Etag")
+	delete(h, "Last-Modified")
+	delete(h, "Content-Encoding")
+	h["Content-Length"] = []string{s.size}
+	h["Content-Type"] = []string{s.mime}
+
+	w.WriteHeader(s.code)
+
+	if r.Method != http.MethodHead {
+		w.Write(s.content)
+	}
+}
