f66cc4b2ffe68fe0e7196130d7c24950305b826a
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-03 10:39:07 +0930

    Make SNIMatch into function (not struct)

--- sni-match.go
+++ sni-match.go
@@ -13,30 +13,34 @@ import (
 // SNIMatch verifies that the TLS SNI extension
 // matches the HTTP Host header.
 //
-// It does nothing for HTTP/2.0 to allow
-// for connection coalescing.
-type SNIMatch struct {
-	http.Handler
+// It does nothing for HTTP/2.0 to allow for
+// connection coalescing.
+//
+// mismatch is invoked on requests that contain
+// a mismatch between the TLS SNI extension and
+// the HTTP Host header.
+//
+// If Mismatch is nil, a 400 Bad Request error
+// will be returned instead.
+func SNIMatch(h http.Handler, mismatch http.Handler) http.Handler {
+	if mismatch == nil {
+		mismatch = ErrorCode(http.StatusBadRequest)
+	}
 
-	// Mismatch is invoked on requests
-	// that contain a mismatch between the
-	// TLS SNI extension and the HTTP Host
-	// header.
-	//
-	// If Mismatch is nil, a 400 Bad
-	// Request error will be returned instead.
-	Mismatch http.Handler
+	return &sniMatch{h, mismatch}
 }
 
-// ServeHTTP implements http.Handler.
-func (h *SNIMatch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch {
-	case r.TLS == nil || r.TLS.ServerName == "" || r.ProtoMajor == 2 ||
-		r.TLS.ServerName == (&url.URL{Host: r.Host}).Hostname():
-		h.Handler.ServeHTTP(w, r)
-	case h.Mismatch != nil:
-		h.Mismatch.ServeHTTP(w, r)
-	default:
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+type sniMatch struct {
+	http.Handler
+
+	mismatch http.Handler
+}
+
+func (m *sniMatch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.TLS == nil || r.TLS.ServerName == "" || r.ProtoMajor == 2 ||
+		r.TLS.ServerName == (&url.URL{Host: r.Host}).Hostname() {
+		m.Handler.ServeHTTP(w, r)
+	} else {
+		m.mismatch.ServeHTTP(w, r)
 	}
 }
