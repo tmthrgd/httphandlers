546a2b81eb351e5e620cad1ced9d028e4e9c8132
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-11-15 15:39:53 +1030

    Add test case for HostRedirect

---
+++ host-redirector_test.go
@@ -0,0 +1,37 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License that can be found in
+// the LICENSE file.
+
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHostRedirector(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/path/to/file", nil)
+	w := httptest.NewRecorder()
+	HostRedirect("example.com", http.StatusSeeOther).ServeHTTP(w, r)
+
+	assert.Equal(t, w.Code, http.StatusSeeOther)
+	assert.Equal(t, w.HeaderMap.Get("Location"), "http://example.com/path/to/file")
+
+	r = httptest.NewRequest(http.MethodGet, "https://example.org/path/to/file", nil)
+	w = httptest.NewRecorder()
+	HostRedirect("example.com", http.StatusSeeOther).ServeHTTP(w, r)
+
+	assert.Equal(t, w.Code, http.StatusSeeOther)
+	assert.Equal(t, w.HeaderMap.Get("Location"), "https://example.com/path/to/file")
+
+	r = httptest.NewRequest(http.MethodGet, "https://example.org:1234/path/to/file", nil)
+	w = httptest.NewRecorder()
+	HostRedirect("example.com", http.StatusSeeOther).ServeHTTP(w, r)
+
+	assert.Equal(t, w.Code, http.StatusSeeOther)
+	assert.Equal(t, w.HeaderMap.Get("Location"), "https://example.com:1234/path/to/file")
+}
